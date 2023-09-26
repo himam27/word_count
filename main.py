a77b58303d34c75c884766fa3eed0cf497b3d5ba
Mathéo Hima <himam@educ.college-montreal.qc.ca>
2023-09-26 11:11:03 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Par Mathéo Hima
+#Fait en 2023
+#Code TP1
+#Début du code
+def count_words (phrase) : #création fonction
+  nombre_mots = (len(phrase.split(" ")), "mots") # len et split compte le nombre de mots
+  return nombre_mots #retourne la fonction
